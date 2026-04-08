8997be2c3fb1f536c25c67fc9e6c8cbc18bde01c
xiong <xiong@comet.la>
2026-04-08 16:26:40 +0700

    import fix

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Logo from "@/public/Logo1.png";
 import Link from "next/link";
 import { useTaskCounts } from "@/hooks/useTaskCounts";
-import React, { useState, useEffect } from "react";
 import { Menu, X, LayoutDashboard, Timer, FileText, BookOpen, CheckSquare, ListTodo, PlusSquare, ImageIcon } from "lucide-react";
 import { TbRobot } from "react-icons/tb";
 import { onAuthStateChanged, signOut } from "firebase/auth";
